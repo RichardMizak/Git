97eef39322d03cbde1773bd42c47bf5c9a4c41ac
kosickaakademia <72263463+kosickaakademia@users.noreply.github.com>
2021-02-19 19:41:57 +0100

    Update Hello.java
    changed

--- Hello.java
+++ Hello.java
@@ -1,5 +1,16 @@
+// super trieda
 public class MyFirstJavaProgram {
+   // mohlo tu by trosku viac kody
+   /* mali ste dat dva subory
+   */
+   
    public static void main(String []args) {
-      System.out.println("Hello World"); 
+      
+      System.out.println("Hello World - Hi"); 
    }
+   
+   public void hello(){
+      // ty je vypis : hello
+   }
+   
 }
